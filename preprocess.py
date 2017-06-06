e8f8db4e1e2af234c6f21320fa43e2d32a6d757a
williamd4112 <williamd@hotmail.com>
2017-06-06 15:51:20 +0800

    modifiy caffe compatability

--- preprocess.py
+++ preprocess.py
@@ -42,9 +42,12 @@ if __name__ == '__main__':
     path = sys.argv[1]
     mean_path = sys.argv[2]
     
-    mean = np.zeros([84, 84, 3], dtype=np.float32)
+    mean = np.zeros([84, 84, 3], dtype=np.float64)
+    n = 0
     for path in tqdm(glob.glob(os.path.join(path, '*.tfrecords'))):
         reader = EpisodeReader(path)
         for s, a, x in reader.read():
             mean += s[:,:,-3:]
+            n += 1
+    mean /= n
     np.save(mean_path, mean)
